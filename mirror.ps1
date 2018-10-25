68926faf5400bd892fa7569d4eeffed0a3ae6921
Frigo <frigocoder@gmail.com>
2018-10-25 14:47:06 +0200

    Print out source and target drive.

--- mirror.ps1
+++ mirror.ps1
@@ -1,19 +1,37 @@
-[CmdletBinding()]
-Param (
-    [Parameter(Mandatory = $true)]
-    [ValidateScript( {Test-Path $_ })]
-    [String] $source,
+Function Main {
+    [CmdletBinding()]
+    Param (
+        [Parameter(Mandatory = $true)]
+        [ValidateScript( {Test-Path $_ })]
+        [String] $source,
 
-    [Parameter(Mandatory = $true)]
-    [ValidateScript( {Test-Path $_ })]
-    [String] $target
-)
-Begin {
-    "Begin"
+        [Parameter(Mandatory = $true)]
+        [ValidateScript( {Test-Path $_ })]
+        [String] $target
+    )
+    Begin {
+        "Begin"
+        "Source drive: {0}" -f (Get-Drive $source)
+        "Target drive: {0}" -f (Get-Drive $target)
+    }
+    Process {
+        "Process"
+    }
+    End {
+        "End"
+    }
 }
-Process {
-    "Process"
+
+Function Get-Drive {
+    [CmdletBinding()]   
+    Param (
+        [Parameter(Mandatory = $true)]
+        [ValidateScript( {Test-Path $_ })]
+        [String] $path
+    )
+    Process {
+        return (Get-Item $path).PSDrive.Name + ":"
+    }
 }
-End {
-    "End"
-}
+
+Main @args
