0ba7d29b49077964bcb3e8630e6b8a33d8d38450
Frigo <frigocoder@gmail.com>
2018-10-25 21:19:32 +0200

    Removed validation because Test-Path and Test-Drive are buggy as hell. Added mounting, unfortunately non-functional at the moment.

--- mirror.ps1
+++ mirror.ps1
@@ -2,26 +2,25 @@ Function Main {
     [CmdletBinding()]
     Param (
         [Parameter(Mandatory = $true)]
-        [ValidateScript( {Test-Path $_ })]
         [String] $source,
 
         [Parameter(Mandatory = $true)]
-        [ValidateScript( {Test-Path $_ })]
         [String] $target,
 
         [Parameter(Mandatory = $false)]
-        [ValidateScript( {!(Test-Path $_)})]
-        [String] $shadowDrive = "B:"
+        [String] $shadowDrive = "B"
     )
     Begin {
         $sourceDrive = Get-Drive $source
         $targetDrive = Get-Drive $target
         $shadow = New-Shadow $sourceDrive
+        New-Mount $shadowDrive $shadow.DeviceObject
     }
     Process {
-        "Process"
+        dir $shadowDrive
     }
     End {
+        Remove-Mount $shadowDrive
         Remove-Shadow $shadow
     }
 }
@@ -30,24 +29,22 @@ Function Get-Drive {
     [CmdletBinding()]
     Param (
         [Parameter(Mandatory = $true)]
-        [ValidateScript( {Test-Path $_ })]
         [String] $path
     )
     Process {
-        return (Get-Item $path).PSDrive.Name + ":"
+        return (Get-Item $path).PSDrive.Name
     }
 }
 
 Function New-Shadow {
-    [CmdLetBinding()]
+    [CmdletBinding()]
     Param (
         [Parameter(Mandatory = $true)]
-        [ValidateScript( {Test-Path $_})]
         [String] $drive
     )
     Process {
         $Win32_ShadowCopy = Get-WmiObject -List Win32_ShadowCopy
-        $created = $Win32_ShadowCopy.Create($drive + "\", "ClientAccessible")
+        $created = $Win32_ShadowCopy.Create($drive + ":\", "ClientAccessible")
         $shadow = Get-WmiObject Win32_ShadowCopy | Where-Object ID -eq $created.ShadowId
         Write-Verbose $shadow
         Write-Verbose $shadow.DeviceObject
@@ -56,12 +53,38 @@ Function New-Shadow {
 }
 
 Function Remove-Shadow {
-    [CmdLetBinding()]
+    [CmdletBinding()]
     Param (
+        [Parameter(Mandatory = $true)]
         $shadow
     )
     Process {
         $shadow.Delete()
+    }
+}
+
+Function New-Mount {
+    [CmdletBinding()]
+    Param (
+        [Parameter(Mandatory = $true)]
+        [String] $drive,
+
+        [Parameter(Mandatory = $true)]
+        [String] $path
+    )
+    Process {
+        New-PsDrive -Name $drive -PSProvider "FileSystem" -Root $path
+    }
+}
+
+Function Remove-Mount {
+    [CmdletBinding()]
+    Param (
+        [Parameter(Mandatory = $true)]
+        [String] $drive
+    )
+    Process {
+        Get-PSDrive $drive | Remove-PSDrive
     }
 }
 
