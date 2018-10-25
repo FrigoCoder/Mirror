f353385d34a010b6c41bfba81dd2324610126c5c
Frigo <frigocoder@gmail.com>
2018-10-25 17:58:22 +0200

    Create and delete shadow copy.

--- mirror.ps1
+++ mirror.ps1
@@ -7,23 +7,29 @@ Function Main {
 
         [Parameter(Mandatory = $true)]
         [ValidateScript( {Test-Path $_ })]
-        [String] $target
+        [String] $target,
+
+        [Parameter(Mandatory = $false)]
+        [ValidateScript( {!(Test-Path $_)})]
+        [String] $shadowDrive = "B:"
     )
     Begin {
-        "Begin"
-        "Source drive: {0}" -f (Get-Drive $source)
-        "Target drive: {0}" -f (Get-Drive $target)
+        $sourceDrive = Get-Drive $source
+        $targetDrive = Get-Drive $target
+        $shadow = New-Shadow $sourceDrive
     }
     Process {
         "Process"
     }
     End {
-        "End"
+        Remove-Shadow $shadow
+        $shadows = Get-WmiObject Win32_ShadowCopy
+        "There are now {0} shadow copies on this sytem" -f $shadows.count
     }
 }
 
 Function Get-Drive {
-    [CmdletBinding()]   
+    [CmdletBinding()]
     Param (
         [Parameter(Mandatory = $true)]
         [ValidateScript( {Test-Path $_ })]
@@ -31,6 +37,32 @@ Function Get-Drive {
     )
     Process {
         return (Get-Item $path).PSDrive.Name + ":"
+    }
+}
+
+Function New-Shadow {
+    [CmdLetBinding()]
+    Param (
+        [Parameter(Mandatory = $true)]
+        [ValidateScript( {Test-Path $_})]
+        [String] $drive
+    )
+    Process {
+        $Win32_ShadowCopy = Get-WmiObject -List Win32_ShadowCopy
+        $created = $Win32_ShadowCopy.Create($drive + "\", "ClientAccessible")
+        $shadow = Get-WmiObject Win32_ShadowCopy | Where-Object ID -eq $created.ShadowId
+        Write-Verbose $shadow
+        return $shadow
+    }
+}
+
+Function Remove-Shadow {
+    [CmdLetBinding()]
+    Param (
+        $shadow
+    )
+    Process {
+        $shadow.Delete()
     }
 }
 
