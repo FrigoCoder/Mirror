ecd46438ff78eb05f7ddf57e55be9f5d50590460
Frigo <frigocoder@gmail.com>
2018-10-28 21:27:04 +0100

    Revert "Try FileShareBackup context instead of ClientAccessible."
    This reverts commit 157d957df38e677a53a5c5033dbf612d53fd0c1d.

--- Mirror.ps1
+++ Mirror.ps1
@@ -54,7 +54,7 @@ Function New-Shadow {
     )
     Process {
         $Win32_ShadowCopy = Get-WmiObject -List Win32_ShadowCopy
-        $created = $Win32_ShadowCopy.Create($drive + "\", "FileShareBackup")
+        $created = $Win32_ShadowCopy.Create($drive + "\", "ClientAccessible")
         $shadow = Get-WmiObject Win32_ShadowCopy | Where-Object ID -eq $created.ShadowId
         Write-Verbose $shadow
         Write-Verbose $shadow.DeviceObject
